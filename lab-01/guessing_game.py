1995f188ff735aa605867a726a5472c8b01ce0aa
Nishtha Kumar <Nishtha.Kumar@seattlecolleges.edu>
2024-08-12 13:17:58 -0700

    Update guessing_game.py

--- lab-01/guessing_game.py
+++ lab-01/guessing_game.py
@@ -1,28 +1,26 @@
 import random
 
-# Display a welcome message
-print("Welcome to the Guessing Game!")
-print("I have chosen a number between 1 and 25. Can you guess what it is?")
-
-# Generate a random number between 1 and 25
-secret_number = random.randint(1, 25)
-
-# Initialize the user's guess to a value outside the possible range
-user_guess = 0
-
-# Start the guessing loop
-while user_guess != secret_number:
-    try:
-        # Prompt the user to guess
-        user_guess = int(input("Enter your guess: "))
+def guessing_game():
+    print("Welcome to the Guessing Game!")
+    print("I'm thinking of a number between 1 and 25.")
+    
+    # Generate a random number between 1 and 25
+    target_number = random.randint(1, 25)
+    guessed_correctly = False
+    
+    # Use a while loop to allow the user to guess repeatedly
+    while not guessed_correctly:
+        guess = int(input("Enter your guess: "))
         
-        # Provide a hint if the guess is incorrect
-        if user_guess < secret_number:
-            print("Too low! Try again.")
-        elif user_guess > secret_number:
-            print("Too high! Try again.")
+        # Provide feedback about the guess
+        if guess == target_number:
+            print("Congratulations! You guessed the number correctly!")
+            guessed_correctly = True
+        elif guess < target_number:
+            print("Too low! Try guessing a higher number.")
         else:
-            print("Congratulations! You've guessed the correct number.")
-    except ValueError:
-        print("Please enter a valid number between 1 and 25.")
+            print("Too high! Try guessing a lower number.")
+
+# Call the function to start the game
+guessing_game()
 

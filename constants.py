7f164c6ba7d0d68bc39bcc353c19d7bf8a655270
Nishtha Kumar <Nishtha.Kumar@seattlecolleges.edu>
2024-08-12 00:56:05 -0700

    Delete constants.py

--- constants.py
+++
@@ -1,8 +0,0 @@
-# Gravitational constants for various planets
-
-earth_g = 9.81  # m/s^2
-mars_g = 3.71  # m/s^2
-
-if __name__ == '__main__':
-    print('Earth constant:', earth_g)
-    print('Mars constant:', mars_g)

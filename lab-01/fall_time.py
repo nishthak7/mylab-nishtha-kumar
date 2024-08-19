b24789c762e0b0029bbe5f20931ee2439b24c4c9
nishthakumar7 <Nishtha.Kumar@seattlecolleges.edu>
2024-08-19 11:53:00 -0700

    add main

--- lab-01/fall_time.py
+++ lab-01/fall_time.py
@@ -1,8 +1,10 @@
+# Import the constants file and math module
 import math
 from constants import earth_g, mars_g
 
-height = int(input('Height in meters: '))  # Meters from planet
+height = float(input('Height in meters: '))  # Meters from planet (now allows decimals)
 
+# Calculate and print out time values for Earth and Mars, rounded to two decimal places
 if __name__ == '__main__':
-    print('Earth:', math.sqrt(2 * height / earth_g), 'seconds')
-    print('Mars:', math.sqrt(2 * height / mars_g), 'seconds')
+    print(f'Earth: {math.sqrt(2 * height / earth_g):.2f} seconds')
+    print(f'Mars: {math.sqrt(2 * height / mars_g):.2f} seconds')
